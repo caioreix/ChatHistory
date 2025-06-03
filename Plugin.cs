349f15b73d5fab1f8d87c6e88ff332af5b89ebf0
Caio Almeida <caioreix@outlook.com>
2025-06-03 20:33:32 -0300

    refactor: streamline key binds unregistration

--- Plugin.cs
+++ Plugin.cs
@@ -2,12 +2,8 @@
 using BepInEx.Unity.IL2CPP;
 using ChatHistory.Systems;
 using HarmonyLib;
-using Il2CppInterop.Runtime.Injection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using UnityEngine;
+using Utils.Injection;
 using Utils.VRising.Entities;
 
 namespace ChatHistory;
@@ -18,32 +14,6 @@ public class Plugin : BasePlugin {
     private static Harmony harmony;
     private static KeyBindsBehaviour keyBindsBehaviour;
 
-    private static void UnregisterKeyBindsBehaviour() {
-        if (ClassInjector.IsTypeRegisteredInIl2Cpp<KeyBindsBehaviour>()) {
-            {
-                var field = typeof(ClassInjector).GetField("InjectedTypes", BindingFlags.NonPublic | BindingFlags.Static);
-                var value = field.GetValue(null);
-                var InjectedTypes = value as HashSet<string>;
-                InjectedTypes?.Remove(typeof(KeyBindsBehaviour).FullName);
-            }
-
-            {
-                var helpersType = typeof(ClassInjector).Assembly.GetType("Il2CppInterop.Runtime.Injection.InjectorHelpers");
-                var field = helpersType.GetField("s_ClassNameLookup", BindingFlags.NonPublic | BindingFlags.Static);
-                var value = field.GetValue(null);
-                if (value is Dictionary<(string _namespace, string _class, IntPtr imagePtr), IntPtr> s_ClassNameLookup) {
-                    var type = typeof(KeyBindsBehaviour);
-                    string klass = type.Name;
-                    string namespaze = type.Namespace ?? string.Empty;
-
-                    s_ClassNameLookup.Keys.Where(key => key._class == klass && key._namespace == namespaze).ToList().ForEach(key => {
-                        s_ClassNameLookup.Remove(key);
-                    });
-                }
-            }
-        }
-    }
-
     public override void Load() {
         if (!World.IsClient) {
             return;
@@ -51,7 +21,7 @@ public class Plugin : BasePlugin {
 
         Settings.Config.Load(Config, Log, "Client");
 
-        UnregisterKeyBindsBehaviour();
+        IL2CPP.Unregister<KeyBindsBehaviour>();
         keyBindsBehaviour = AddComponent<KeyBindsBehaviour>();
 
         harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
@@ -68,7 +38,7 @@ public class Plugin : BasePlugin {
 
         harmony.UnpatchSelf();
         GameObject.Destroy(keyBindsBehaviour);
-        UnregisterKeyBindsBehaviour();
+        IL2CPP.Unregister<KeyBindsBehaviour>();
 
         Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} v{MyPluginInfo.PLUGIN_VERSION} client side is unloaded!");
         return true;
